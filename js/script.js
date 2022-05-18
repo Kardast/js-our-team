bf4532f30a60563a473e0d0a6df00bccb6b33440
Kardast <dottorsf@gmail.com>
2022-05-18 16:26:53 +0200

    Cycle

--- js/script.js
+++ js/script.js
@@ -3,65 +3,75 @@ const memberCards = [
     {
         "Nome" : "Wayne Barnett",
         "Ruolo" : "Founder Ceo",
-        "Foto" : "./img/wayne-barnett-founder-ceo.jpg"
+        "Foto" : "img/wayne-barnett-founder-ceo.jpg"
     },
     {
         "Nome" : "Angela Caroll",
         "Ruolo" : "Chief Editor",
-        "Foto" : "./img/angela-caroll-chief-editor.jpg"
+        "Foto" : "img/angela-caroll-chief-editor.jpg"
     },
     {
         "Nome" : "Walter Gordon",
         "Ruolo" : "Office Manager",
-        "Foto" : "./img/walter-gordon-office-manager.jpg"
+        "Foto" : "img/walter-gordon-office-manager.jpg"
     },
     {
         "Nome" : "Angela Lopez",
         "Ruolo" : "Social Media Manager",
-        "Foto" : "./img/angela-lopez-social-media-manager.jpg"
+        "Foto" : "img/angela-lopez-social-media-manager.jpg"
     },
     {
         "Nome" : "Scott Estrada",
         "Ruolo" : "Developer",
-        "Foto" : "./img/scott-estrada-developer.jpg"
+        "Foto" : "img/scott-estrada-developer.jpg"
     },
     {
         "Nome" : "Barbara Ramos",
         "Ruolo" : "Graphic Designer",
-        "Foto" : "./img/barbara-ramos-graphic-designer.jpg"
+        "Foto" : "img/barbara-ramos-graphic-designer.jpg"
     },
 ]
-
-console.log(memberCards);
+// console.log(memberCards);
 
 let printCard = document.querySelector(".team-container");
-console.log(printCard);
+// console.log(printCard);
 
-let divCard = document.createElement("div");
-divCard.classList.add("team-card");
-printCard.append(divCard);
+for (let i = 0; i < memberCards.length; i++) {
+    // const element = memberCards[i];
 
-let divImage = document.createElement("div");
-divImage.classList.add("card-image");
-divCard.append(divImage);
+    let divCard = document.createElement("div");
+    divCard.classList.add("team-card");
+    printCard.append(divCard);
 
-let imgTag = document.createElement("img");
-imgTag.src = "img/wayne-barnett-founder-ceo.jpg";
-imgTag.alt = "Wayne Barnett";
-divImage.append(imgTag);
+    let divImage = document.createElement("div");
+    divImage.classList.add("card-image");
+    divCard.append(divImage);
 
-let divText = document.createElement("div");
-divText.classList.add("card-text");
-divCard.append(divText);
+    let imgTag = document.createElement("img");
+    imgTag.src = "img/wayne-barnett-founder-ceo.jpg";
+    imgTag.alt = "Wayne Barnett";
+    divImage.append(imgTag);
 
-let h3Tag = document.createElement("h3");
-divText.append(h3Tag);
-let h3Text = document.createTextNode("prova");
-h3Tag.append(h3Text);
+    let divText = document.createElement("div");
+    divText.classList.add("card-text");
+    divCard.append(divText);
 
-let pTag = document.createElement("p");
-divText.append(pTag);
-let pText = document.createTextNode("prova2");
-pTag.append(pText);
+    let h3Tag = document.createElement("h3");
+    divText.append(h3Tag);
+    let h3Text = document.createTextNode("prova");
+    h3Tag.append(h3Text);
+
+    let pTag = document.createElement("p");
+    divText.append(pTag);
+    let pText = document.createTextNode("prova2");
+    pTag.append(pText);
+
+    console.log(memberCards[i]);
+    
+}
+
+
+
+
 
 
