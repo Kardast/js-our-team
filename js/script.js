1a57373c6171e53e5520b61f512ef382b31e98b0
Kardast <dottorsf@gmail.com>
2022-05-18 17:24:12 +0200

    Working on keys

--- js/script.js
+++ js/script.js
@@ -29,44 +29,22 @@ const memberCards = [
         "Nome" : "Barbara Ramos",
         "Ruolo" : "Graphic Designer",
         "Foto" : "img/barbara-ramos-graphic-designer.jpg"
-    },
-]
+    }
+];
 // console.log(memberCards);
 
 let printCard = document.querySelector(".team-container");
 // console.log(printCard);
 
 for (let i = 0; i < memberCards.length; i++) {
-    // const element = memberCards[i];
 
-    let divCard = document.createElement("div");
-    divCard.classList.add("team-card");
-    printCard.append(divCard);
+    let memberCardsIesimo = memberCards[i];
 
-    let divImage = document.createElement("div");
-    divImage.classList.add("card-image");
-    divCard.append(divImage);
+    for (let key in memberCardsIesimo){
+        console.log(key + ": " + memberCardsIesimo[key]);
 
-    let imgTag = document.createElement("img");
-    imgTag.src = "img/wayne-barnett-founder-ceo.jpg";
-    imgTag.alt = "Wayne Barnett";
-    divImage.append(imgTag);
+    };
 
-    let divText = document.createElement("div");
-    divText.classList.add("card-text");
-    divCard.append(divText);
-
-    let h3Tag = document.createElement("h3");
-    divText.append(h3Tag);
-    let h3Text = document.createTextNode("prova");
-    h3Tag.append(h3Text);
-
-    let pTag = document.createElement("p");
-    divText.append(pTag);
-    let pText = document.createTextNode("prova2");
-    pTag.append(pText);
-
-    console.log(memberCards[i]);
     
 }
 
@@ -75,3 +53,30 @@ for (let i = 0; i < memberCards.length; i++) {
 
 
 
+
+let divCard = document.createElement("div");
+divCard.classList.add("team-card");
+printCard.append(divCard);
+
+let divImage = document.createElement("div");
+divImage.classList.add("card-image");
+divCard.append(divImage);
+
+let imgTag = document.createElement("img");
+imgTag.src = "img/wayne-barnett-founder-ceo.jpg";
+imgTag.alt = "Wayne Barnett";
+divImage.append(imgTag);
+
+let divText = document.createElement("div");
+divText.classList.add("card-text");
+divCard.append(divText);
+
+let h3Tag = document.createElement("h3");
+divText.append(h3Tag);
+let h3Text = document.createTextNode("prova");
+h3Tag.append(h3Text);
+
+let pTag = document.createElement("p");
+divText.append(pTag);
+let pText = document.createTextNode("prova2");
+pTag.append(pText);
