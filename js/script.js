5a39559e83fb658f520a76dda6bdd844301da4fe
Kardast <dottorsf@gmail.com>
2022-05-18 15:12:45 +0200

    Data structure

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const memberCards = [
+
+    {
+        "Nome" : "Wayne Barnett",
+        "Ruolo" : "Founder Ceo",
+        "Foto" : "./img/wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        "Nome" : "Angela Caroll",
+        "Ruolo" : "Chief Editor",
+        "Foto" : "./img/angela-caroll-chief-editor.jpg"
+    },
+    {
+        "Nome" : "Walter Gordon",
+        "Ruolo" : "Office Manager",
+        "Foto" : "./img/walter-gordon-office-manager.jpg"
+    },
+    {
+        "Nome" : "Angela Lopez",
+        "Ruolo" : "Social Media Manager",
+        "Foto" : "./img/angela-lopez-social-media-manager.jpg"
+    },
+    {
+        "Nome" : "Scott Estrada",
+        "Ruolo" : "Developer",
+        "Foto" : "./img/scott-estrada-developer.jpg"
+    },
+    {
+        "Nome" : "Barbara Ramos",
+        "Ruolo" : "Graphic Designer",
+        "Foto" : "./img/barbara-ramos-graphic-designer.jpg"
+    },
+]
+
+console.log(memberCards);
+
